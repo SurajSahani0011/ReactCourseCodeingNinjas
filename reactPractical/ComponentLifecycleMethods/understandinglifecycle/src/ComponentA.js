c569fc1a76fc50c6b8b388ed2828f7ba88e9aa88
Suraj sahani <116185940+SurajSahani0011@users.noreply.github.com>
2023-07-12 06:35:38 +0530

    some added

---
+++ reactPractical/ComponentLifecycleMethods/understandinglifecycle/src/ComponentA.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ComponentB from "./ComponentB";
+class ComponentA extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            name: "ComponentA",
+            data: []
+        }
+        console.log("ComponentA Constructor")
+    }
+
+    static getDerivedStateFromProps(props,state){
+        console.log("ComponentA getDerivedStateByprops");
+        return null;
+    }
+    
+    //https://jsonplaceholder.typicode.com/users
+
+    componentDidMount(){
+        console.log("ComponentA componentDidMount");
+        fetch('https://jsonplaceholder.typicode.com/users')
+         .then(response => response.json())
+         .then(data => this.setState({data}));
+    }
+    render(){
+        
+
+
+        return(<>
+              <h1>{this.state.name}</h1>
+              <ul>
+                {this.state.data.map((d)=>{
+                    return(
+                        <li>{d.username}</li>
+                    )
+                })}
+              </ul>
+              
+              
+              </>)
+    }
+
+}
+
+export default ComponentA;
---
+++ reactPractical/ComponentLifecycleMethods/understandinglifecycle/src/ComponentB.js
@@ -0,0 +1,27 @@
+import React from "react";
+class ComponentB extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            name: "ComponentB"
+        }
+        console.log("ComponentB Constructor")
+    }
+
+    static getDerivedStateFromProps(props,state){
+        console.log("ComponentB getDerivedStateByprops");
+        return null;
+    }
+
+    componentDidMount(){
+        console.log("ComponentB componentDidMount")
+    }
+    render(){
+        console.log("ComponentB render")
+        return(<h2>{this.state.name}</h2>)
+    }
+
+}
+
+export default ComponentB;
